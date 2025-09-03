ba1efc23997b4443ddb7dd3c693a70c55f17ea7e
HarukiIsono <haruki.isono861@gmail.com>
2025-09-03 21:07:13 +0900

    fix

--- config/lucia_lds_2d.lua
+++ config/lucia_lds_2d.lua
@@ -7,7 +7,7 @@ options = {
 
   map_frame = "map",
   tracking_frame = "base_link",
-  published_frame = "base_footprint",
+  published_frame = "base_link",
   odom_frame = "odom",
 
   provide_odom_frame = false,
@@ -17,7 +17,7 @@ options = {
   use_nav_sat = false,
   use_landmarks = false,
 
-  -- LiDARが1台なら 1、2台運用時のみ 2 にする
+  -- LiDAR が1台なら 1、2台なら 2（Remap 必須）
   num_laser_scans = 2,
   num_multi_echo_laser_scans = 0,
   num_subdivisions_per_laser_scan = 1,
@@ -31,14 +31,15 @@ options = {
   rangefinder_sampling_ratio = 1.,
   odometry_sampling_ratio = 1.,
   fixed_frame_pose_sampling_ratio = 1.,
-  -- imu_sampling_ratio と landmarks_sampling_ratio は定義しない（未使用キーでクラッシュ回避）
+  imu_sampling_ratio = 1.,
+  landmarks_sampling_ratio = 1.,
 }
 
 MAP_BUILDER.use_trajectory_builder_2d = true
 MAP_BUILDER.use_trajectory_builder_3d = false
 MAP_BUILDER.num_background_threads = 4
 
--- ループ閉じの発見率を上げる
+-- ループ閉じ検出を強める
 POSE_GRAPH.optimize_every_n_nodes = 90
 POSE_GRAPH.constraint_builder.sampling_ratio = 0.9
 POSE_GRAPH.constraint_builder.max_constraint_distance = 25.0
@@ -51,17 +52,18 @@ POSE_GRAPH.constraint_builder.loop_closure_translation_weight = 1.1e4
 POSE_GRAPH.constraint_builder.loop_closure_rotation_weight = 1e5
 POSE_GRAPH.constraint_builder.log_matches = true
 
--- マッチャの重み（必要なら調整）
+-- マッチャの重み
 POSE_GRAPH.matcher_translation_weight = 5e2
 POSE_GRAPH.matcher_rotation_weight = 1.6e3
 
--- 誤ループ抑制。強すぎるとループ効果が出ないので 1e2 程度から
+-- Odometry あり想定。強すぎるとループ効果が出ないため 1e2 から
 POSE_GRAPH.optimization_problem.local_slam_pose_translation_weight = 1e2
-POSE_GRAPH.optimization_problem.local_slam_pose_rotation_weight = 1e2
-POSE_GRAPH.optimization_problem.odometry_translation_weight = 1e2
-POSE_GRAPH.optimization_problem.odometry_rotation_weight = 1e2
+POSE_GRAPH.optimization_problem.local_slam_pose_rotation_weight   = 1e2
+POSE_GRAPH.optimization_problem.odometry_translation_weight       = 1e2
+POSE_GRAPH.optimization_problem.odometry_rotation_weight          = 1e2
 POSE_GRAPH.optimization_problem.huber_scale = 1e1
 
+-- 2D ビルダー設定
 TRAJECTORY_BUILDER_2D.use_imu_data = false
 TRAJECTORY_BUILDER_2D.min_range = 0.2
 TRAJECTORY_BUILDER_2D.max_range = 15.0
@@ -73,5 +75,10 @@ TRAJECTORY_BUILDER_2D.submaps.num_range_data = 90
 TRAJECTORY_BUILDER_2D.submaps.grid_options_2d.resolution = 0.05
 TRAJECTORY_BUILDER_2D.motion_filter.max_distance_meters = 0.1
 TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(0.2)
+
+-- ローカル相関（必要時のみ有効化）
+-- TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
+-- TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.2
+-- TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.angular_search_window = math.rad(20.)
 
 return options
